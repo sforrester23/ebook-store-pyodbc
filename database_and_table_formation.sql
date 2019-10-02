f6ec73d19d4fd82446fe33dd4fa8afcdbc6f9355
Sam Forrester <SForrester@spartaglobal.com>
2019-10-02 23:34:28 +0100

    Added the SQL file that made the database we played around with/connected to

---
+++ database_and_table_formation.sql
@@ -0,0 +1,18 @@
+--CREATE DATABASE ebook_store_db
+
+USE ebook_store_db;
+
+--CREATE TABLE books
+--(
+--book_title VARCHAR(50),
+--author VARCHAR(50),
+--date_published DATE
+--)
+
+--INSERT INTO books
+--VALUES
+--('Cloud Atlas', 'David Mitchell', '2004/03/01'), ('The Martian', 'Andy Weir', '2011/05/19'), ('Snow Falling on Cedars', 'David Guterson', '1994/09/05'), ('Gone Girl',	'Gillian Flynn', '2012/05/24'), ('The Haunting of Hill House', 'Shirley Jackson', '1959/02/28')
+
+--DELETE FROM books WHERE book_title = 'Of Mice and Men'
+
+SELECT * FROM books
