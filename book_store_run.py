b22b79126e4c110648df2c0859187afcde3e78e9
Sam Forrester <SForrester@spartaglobal.com>
2019-10-02 23:17:14 +0100

    added some more methods as per user spec. incl. method to print all book info, method to search for a book title and method to change data in a database - in this instance add a book

--- book_store_run.py
+++ book_store_run.py
@@ -7,6 +7,9 @@ password = 'Passw0rd2018'
 
 book_store = Ebook_Store(server, database, username, password)
 
-book_store.print_all_book_info()
+# book_store.print_all_book_info()
+#
+# print(book_store.search_book_title("Cloud Atlas"))
 
-print(book_store.search_book_title("Cloud Atlas"))
+book_store.add_book_to_table("Of Mice and Men", "John Steinbeck", "1955/04/01")
+book_store.update_book_in_table('date_published', '1937', 'book_title', 'Of Mice and Men')
--- connect_book_database.py
+++ connect_book_database.py
@@ -27,4 +27,11 @@ class Ebook_Store():
         title_query = self.__filter_query("SELECT * FROM books WHERE book_title = '{}'".format(str(book_title)))
         return title_query.fetchone()
 
+    def add_book_to_table(self, book_title, book_author, date_published):
+        self.__filter_query("INSERT INTO books VALUES ('{}', '{}', '{}')".format(book_title, book_author, date_published))
+        self.connect_db.commit()
+
+    def update_book_in_table(self, value_to_update, new_value, condition_column, condition_value):
+        self.__filter_query("UPDATE books SET {} = '{}' WHERE {} = '{}'".format(value_to_update, new_value, condition_column, condition_value))
+        self.connect_db.commit()
 
